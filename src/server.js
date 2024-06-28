9cda491375a7c5f58f15a54a0312063a49d50ec1
Suellen Castro <castrosuellenx@gmail.com>
2024-06-28 16:02:39 -0300

    implement database

--- src/server.js
+++ src/server.js
@@ -1,36 +1,31 @@
 import http from 'node:http';
+import {randomUUID} from 'node:crypto';
+import {json} from './middlewares/json.js';
+import {Database} from './database.js';
 
-const users = [];
+const database = new Database();
 
 const server = http.createServer(async (request, response) => {
   const {method, url, headers} = request;
 
-  const buffers = [];
-
-  for await (const chunk of request) {
-    buffers.push(chunk);
-  }
-
-  try {
-    request.body = JSON.parse(Buffer.concat(buffers).toString());
-  } catch (error) {
-    request.body = null;
-  }
+  await json(request, response);
 
   if (method === 'GET' && url === '/users') {
-    return response
-      .setHeader('Content-type', 'application/json')
-      .end(JSON.stringify(users));
+    const users = database.select('users');
+
+    return response.end(JSON.stringify(users));
   }
 
   if (method === 'POST' && url === '/users') {
     const {name, email} = request.body;
 
-    users.push({
-      id: 1,
+    const user = {
+      id: randomUUID(),
       name,
       email,
-    });
+    };
+
+    database.insert('users', user);
 
     return response.writeHead(201).end();
   }
