fdadc9ca878dbd64b616999ea073aab8f9d48349
castrosuellenx <castrosuellenx@gmail.com>
2024-07-03 12:11:04 -0300

    add routes with route params

--- src/server.js
+++ src/server.js
@@ -1,33 +1,23 @@
 import http from 'node:http';
-import {randomUUID} from 'node:crypto';
-import {json} from './middlewares/json.js';
-import {Database} from './database.js';
 
-const database = new Database();
+import {json} from './middlewares/json.js';
+import {routes} from './routes.js';
 
 const server = http.createServer(async (request, response) => {
   const {method, url, headers} = request;
 
   await json(request, response);
 
-  if (method === 'GET' && url === '/users') {
-    const users = database.select('users');
+  const route = routes.find((route) => {
+    return route.method === method && route.path.test(url);
+  });
 
-    return response.end(JSON.stringify(users));
-  }
+  if (route) {
+    const routeParams = request.url.match(route.path);
 
-  if (method === 'POST' && url === '/users') {
-    const {name, email} = request.body;
+    request.params = {...routeParams.groups};
 
-    const user = {
-      id: randomUUID(),
-      name,
-      email,
-    };
-
-    database.insert('users', user);
-
-    return response.writeHead(201).end();
+    return route.handler(request, response);
   }
 
   return response.writeHead(404).end();
